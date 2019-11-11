01a555066f1942de18c20b47b4364d026c16c992
Olgerd_Seviakov <olgerd@zenitech.co.uk>
2019-11-11 11:34:46 +0200

    revert console.log

--- src/config/endpoints.ts
+++ src/config/endpoints.ts
@@ -1,7 +1,6 @@
-const baseUrl = 'https://7ycgcndqo8.execute-api.eu-west-2.amazonaws.com/prod' || process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const baseUrl = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 export const helloWorldApi = baseUrl + '/helloWorld';
 export const getAllReceiptsApi = baseUrl + '/receipt';
 export const editReceiptApi = baseUrl + '/receipt';
 export const createReceiptApi = baseUrl + '/receipt';
 export const deleteReceiptApi = (id: string) => `${baseUrl}/receipt/${id}`;
-console.log('env: ', process.env);
