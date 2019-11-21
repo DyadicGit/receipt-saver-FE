c6c9db737e26cbcc36ad8e7de0a58d296cfb3019
Olgerd_Seviakov <olgerd@zenitech.co.uk>
2019-11-21 14:41:31 +0200

    update environment files

--- src/config/endpoints.ts
+++ src/config/endpoints.ts
@@ -1,4 +1,4 @@
-const baseUrl = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const baseUrl = process.env.REACT_APP_API_URL;
 export const helloWorldApi = baseUrl + '/helloWorld';
 export const getAllReceiptsApi = baseUrl + '/receipt';
 export const editReceiptApi = baseUrl + '/receipt';
